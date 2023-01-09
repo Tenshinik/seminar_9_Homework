df7e071d958f05ae0a8d77eaec99e026984eca7b
Tenshinik <@tenshinik@gmail.com>
2023-01-09 11:26:21 +0800

    exampl3 project

---
+++ examples/examp3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.WriteLine("Даны два числа");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+
+int Accerman(int n, int m)
+{
+    if(n == 0)
+    {
+       return m + 1;
+    }
+    if(m == 0)
+    {
+        return Accerman(n - 1, 1);
+    }
+    return Accerman(n - 1, Accerman(n, m - 1));
+}
+Console.WriteLine(Accerman(n, m));
